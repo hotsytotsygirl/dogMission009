082c2ee505b03cde0a40edd76e0ce21647475c78
Dara Kosova <darakosova.gb@gmail.com>
2022-06-25 00:47:12 +0400

    MY FIRST INDEPENDENT PROGRAM!!!

---
+++ DogAndFriends/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+int distance = 10000;
+int meetingDistance = 10;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while(distance > meetingDistance)
+{
+    if(friend == 1)
+    {
+       time = distance / (firstFriendSpeed + dogSpeed);
+       friend = 2; 
+    }
+
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+
+Console.WriteLine(count);
